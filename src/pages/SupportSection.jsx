31abadb4270603610948f0b0395ae34dfb819193
Gera Carrillo <geracs8001@gmail.com>
2025-02-28 02:25:38 -0600

    correccion de presentacion

--- src/pages/SupportSection.jsx
+++ src/pages/SupportSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
+import styles2 from './intro.css';
 
 const copyEmailToClipboard = () => {
   navigator.clipboard.writeText('ayuda@unineuuni.edu.mx');
@@ -9,7 +10,8 @@ const copyEmailToClipboard = () => {
 function SupportSection() {
   return (
     <div className={styles.supportSection}>
-      <p>Si tienes dudas o sugerencias sobre la plataforma, puedes contactarte a Soporte Técnico:</p>
+      <h2>¿Tienes sugerencias o no encuentras lo que buscas?</h2>
+      <h3>Contáctate con Soporte Técnico: 👇</h3>
       <div className={styles.supportButtons}>
         <a href="https://wa.me/5214401051527" className={styles.whatsappButton} target='_blank' rel='noreferrer'>
           WhatsApp
