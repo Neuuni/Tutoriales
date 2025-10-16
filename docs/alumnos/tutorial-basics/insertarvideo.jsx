5524c8786ddebfd0af9fe6074d318c9fdd196f92
GerCarrillo <g.carrillo@unineuuni.edu.mx>
2025-10-15 18:46:29 -0600

    Pendiente

--- docs/alumnos/tutorial-basics/insertarvideo.jsx
+++ docs/alumnos/tutorial-basics/insertarvideo.jsx
@@ -2,27 +2,21 @@ import React from "react";
 import './intro.css';
 
 function VideoIntro({ title, videoUrl }) {
-  return (
-    <>
-      <hr className="block-div" />
-      <div className="contenedor-video-2">
-        <h1 className="title-intro">{title}</h1>
-      </div>
-      <div className="contenedor-video">
-        <iframe width="560" height="315" src={videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        {/* <iframe
-          className="video"
-          width="580"
-          height="345"
-          src={videoUrl}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe> */}
-      </div>
-    </>
-  );
+  return (
+    <>
+      <div className="contenedor-video">
+        <iframe 
+          className="full-width-video" 
+          src={videoUrl} 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen>
+        </iframe>
+      </div>
+    </>
+  );
 }
 
 export default VideoIntro;
